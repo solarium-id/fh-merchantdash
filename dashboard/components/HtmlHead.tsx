f13b7878913a1ef94729f69c837521bbf3d07bad
Khoirul Asfian <mk.asfian@gmail.com>
2021-04-05 22:20:38 +0700

    add custom font for frontend

--- dashboard/components/HtmlHead.tsx
+++ dashboard/components/HtmlHead.tsx
@@ -11,11 +11,6 @@ function HtmlHead(props: { title: string }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
     </>
   );
--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
+// custom font for chakra-ui
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import Router, { useRouter } from "next/router";
 import { ChakraProvider } from "@chakra-ui/react";
 import NProgress from "nprogress";
 import HtmlHead from "../components/HtmlHead";
 import Layout from "../components/Layout";
+import theme from "../lib/theme";
 
+// nprogress config
 Router.events.on("routeChangeStart", (url: string) => {
   console.log(`Loading: ${url}`);
   NProgress.start();
@@ -12,13 +20,14 @@ Router.events.on("routeChangeStart", (url: string) => {
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
+// main component
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <>
       <HtmlHead title="Next Dashboard" />
 
-      <ChakraProvider resetCSS>
+      <ChakraProvider theme={theme} resetCSS>
         {/* jika menuju route login, tdk perlu merender Layout */}
         {router.pathname === "/login" ? (
           <Component {...pageProps} />
