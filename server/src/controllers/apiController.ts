76ed5b73fa466a6da4e2f9883782875009dd759b
Khoirul Asfian <mk.asfian@gmail.com>
2021-04-06 12:27:07 +0700

    update .env.example for jwt secret

--- server/src/controllers/apiController.ts
+++ server/src/controllers/apiController.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 
 // Get All Controller
 export const GetApiController = (_req: Request, res: Response) => {
-  res.json({ data: "This is the json data" });
+  res.json({ data: "API endpoint ready" });
 };
 
 // Get One Controller
