9a25c9498a1f7ea10574bfce9c9852019ea78c99
Khoirul Asfian <mk.asfian@gmail.com>
2021-04-07 20:41:53 +0700

    install express-fileupload on server

--- dashboard/components/Merchant/NewMerchantForm.tsx
+++ dashboard/components/Merchant/NewMerchantForm.tsx
@@ -18,28 +18,28 @@ function NewMerchantForm() {
     <form>
       <SimpleGrid columns={2} spacing={4}>
         {/* merchantname */}
-        <FormControl id="merchantname">
+        <FormControl isRequired id="merchantname">
           <FormLabel>Nama Merchant</FormLabel>
           <Input type="text" placeholder="Nama Merchant" />
         </FormControl>
         {/* merchantaddr */}
-        <FormControl id="merchantaddr">
+        <FormControl isRequired id="merchantaddr">
           <FormLabel>Alamat Merchant</FormLabel>
           <Input type="text" placeholder="Alamat Merchant" />
         </FormControl>
         {/* merchantph */}
-        <FormControl id="merchantph">
+        <FormControl isRequired id="merchantph">
           <FormLabel>No Telp Merchant</FormLabel>
           <Input type="text" placeholder="08xxxx" />
         </FormControl>
         {/* merchantemail */}
-        <FormControl id="merchantemail">
+        <FormControl isRequired id="merchantemail">
           <FormLabel>Alamat Email Merchant</FormLabel>
           <Input type="text" placeholder="example@email.com" />
         </FormControl>
 
         {/* categoryid */}
-        <FormControl id="categoryid">
+        <FormControl isRequired id="categoryid">
           <FormLabel>Kategori</FormLabel>
           <Select placeholder="Pilih Kategori">
             <option value="category1">Category 1</option>
@@ -48,24 +48,24 @@ function NewMerchantForm() {
           </Select>
         </FormControl>
         {/* ownername */}
-        <FormControl id="ownername">
+        <FormControl isRequired id="ownername">
           <FormLabel>Nama Owner</FormLabel>
           <Input type="text" placeholder="Nama Owner" />
         </FormControl>
         {/* ownerhp */}
-        <FormControl id="ownerhp">
+        <FormControl isRequired id="ownerhp">
           <FormLabel>No Telp Merchant</FormLabel>
           <Input type="text" placeholder="08xxxx" />
         </FormControl>
         {/* owneremail */}
-        <FormControl id="owneremail">
+        <FormControl isRequired id="owneremail">
           <FormLabel>Alamat Email Owner</FormLabel>
           <Input type="text" placeholder="example@email.com" />
         </FormControl>
       </SimpleGrid>
 
       {/* reservation */}
-      <FormControl mt="4" id="reservation">
+      <FormControl isRequired mt="4" id="reservation">
         <FormLabel>Status Reservasi</FormLabel>
         <RadioGroup>
           <Stack direction="row">
@@ -76,12 +76,12 @@ function NewMerchantForm() {
       </FormControl>
 
       {/* merchantpic */}
-      <FormControl mt="4" id="merchantpic">
+      <FormControl isRequired mt="4" id="merchantpic">
         <FormLabel>Foto Merchant</FormLabel>
         <input type="file" />
       </FormControl>
       {/* fotoktp */}
-      <FormControl mt="4" id="merchantpic">
+      <FormControl isRequired mt="4" id="merchantpic">
         <FormLabel>Foto KTP</FormLabel>
         <input type="file" />
       </FormControl>
